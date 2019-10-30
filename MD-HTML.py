4f12e4924f354ad94d5e088572e7b7d8bd1b3e5d
srstrand <57164442+srstrand@users.noreply.github.com>
2019-10-29 20:29:20 -0700

    Add files via upload

---
+++ MD-HTML.py
@@ -0,0 +1,326 @@
+#!/usr/bin/python3
+
+#import libraries
+#================
+#to use some terminal commands (e.g., 'clear')
+import os
+
+import re
+import sys
+#================
+
+#TODO
+#Figure out what to do about extraneous line breaks in the middle of paragraphs
+#Catch escape characters: % (comments), $(math mode!)
+	#Turn $$.*$$ into \[ .* \]
+	#Turn un-escaped $.*$ into \( .* \)
+	#Turn escaped \$ into $
+	#Turn un-escaped % into comments
+	#Turn escaped \% into %
+#optional inclusion of mathjax script if 'math mode' detected in document
+#================
+#Function defs
+def buildList(prefix,tabCount,listLevel,line):
+	#Strip the bullet/digit and space, create list item
+	line = re.sub(r'(^\s*)(?:\+|\-|\*|\d+\.)(?: |\t)(.*)',r'\1\t<li>\2</li>',line)
+
+	#new unordered list or sublist
+	if tabCount[i] == len(listLevel):
+		listLevel.append(prefix)
+		tabs = tabCount[i] #len(listLevel)
+
+		line = re.sub(r'(^\s*)(.*)',r'\t'*tabs+r'<'+prefix+r'l>\n\1\2',line)
+
+	return line
+
+def listWrapup(tabCount,listLevel,line):
+	#Fails if last line of file is a bulleted entry!
+	if emptyLine:
+		#Get rid of the '\n'
+		line = ""
+		tabs = 0
+		while listLevel:
+			prefix = listLevel.pop()
+			tabs = len(listLevel)#tabCount[i]-j
+
+			#The '*' operator allows one to specify a variable-number of occurrences in the replacement string
+			#Since we found an empty line, create closing entries for all open lists. We do this by working backward from the last opened list. regex was the wrong tool for this one!
+			line = line+"\t"*tabs+"</"+prefix+"l>\n"
+		#After conclusion of all open lists, reinsert the empty line
+		line = line + "\n"
+	else:
+		for j in range(tabCount[i],tabCount[i-1]):
+			prefix = listLevel.pop()
+			tabs = j+1
+
+			#The '*' operator allows one to specify a variable-number of occurrences in the replacement string
+			line = re.sub(r'(^\s*)(.*)',r'\t'*tabs+r'</'+prefix+r'l>\n\1\2',line)
+	return line
+
+
+#start a table
+def tableHead(line):
+	return '<table>\n\t<thead>\n\t' + tableEntries('h style="font-weight:bold;"',line) + '\n\t</thead>\n\t<tbody>\n\t'
+
+#create a normal row in the table
+def tableBody(line):
+	return tableEntries("d",line)
+
+
+#Create a row in the table; process table entries separated by pipes; 'data' is either 'h' or 'd' for '<th>' or '<td>', depending on whether or not the current line is a header
+def tableEntries(data,line):
+	columnText = line.split("|")
+	line = ''
+	while columnText:
+		entry = columnText.pop(0)
+
+		#if not empty and not white space, then create column entry
+		if entry and not re.match(r'\s+$',entry):
+			line = line + "\t\t<t" + data +">" + entry + "</t" + data + ">\n"
+	return "<tr>\n" + line + "\n\t</tr>\n\t"
+
+#close up the current table
+def tableClose(line):
+	return line + "\n\t</tbody>\n</table>\n\n"
+
+def insertHeaderInfo(key):
+	return headersDict[key] if headersDict.get(key) else "[missing " + key + "]"
+#================
+
+listLevel = []
+i = 0
+tabCount = [0]
+meta = False
+headersDict = {}
+mathMode = False
+
+fileName = "Syllabus-Template"
+
+with open(fileName + ".md","r") as f:
+	with open(fileName + ".html","w") as g:
+		for line in f:
+			i += 1
+			#search line
+			#Process Hierarchy:
+				# metadata
+				# Headings
+				# lists
+				# tables
+				# strong
+				# emphasis
+				# strikethrough
+				# links
+				# math mode characters
+				# escaped characters: \$, \%
+
+			#Should we wrap the line in <p> tags? Only if it isn't a heading, list, or table
+			p = True
+			tabCount.append(0)
+
+		#Get metadata
+			#Change formatting of metadata section to match yaml, standard markdown
+			if re.match(r'-{3}',line):
+				if meta:
+					meta = False
+				else:
+					meta = True
+				continue
+
+			if meta:
+				key, val = line.split(": ")
+				headersDict[key] = val.rstrip()
+				continue
+
+			#Keep track of indentation level
+			linex = line.expandtabs(4)
+			tabCount[i] = 0 if linex.isspace() else (len(linex) - len(linex.lstrip())) // 4
+
+			#create a tuple of the #s appearing in a line; this determines the 'heading' level in HTML
+			matches = tuple(re.findall(r'\#',line))
+			if matches:
+				p = False
+				#store the number of #s as a string for concatentation
+				level=str(len(matches))
+
+				#if markdown found, replace in 'line'
+				#you can also use '\g<n>' to match the nth saved group
+				#Match all #s and trailing space, then store the rest of the text
+				line = re.sub(r'\#+ (.*)',r'<h'+level+r'>\1</h'+level+r'>',line)
+				
+				#write new 'line' out to file
+				g.write(line)
+
+				#Stop processing the current line
+				continue
+
+		#Lists!
+			#Unordered first
+			if re.match(r'^\s*(?:\+|\-|\*)(?: |\t)',line):
+				p = False
+				line = buildList("u",tabCount,listLevel,line)
+
+			#Now Ordered
+			if re.match(r'^\s*\d+\.(?: |\t)',line):
+				p = False
+				line = buildList("o",tabCount,listLevel,line)
+
+			#end of previous list(s); loop to close all completed lists
+			emptyLine = re.match(r'^\s*$',line)
+			if listLevel and (emptyLine or tabCount[i-1] > tabCount[i]):
+				line = listWrapup(tabCount,listLevel,line)
+
+			if emptyLine:
+				p = False
+
+		#Tables!
+			if re.match(r'\s*\|',line):
+				p = False
+				nextline = ''
+				try:
+					nextline = next(f)
+				except Exception as e:
+					print(e)
+					line = tableClose(tableBody(line))
+					g.write(line)
+					break
+
+				if re.search(r'(?:-){3,}',nextline):
+					#start a table
+					line = tableHead(line)
+
+				elif re.search(r'^\s*$',nextline):
+					#close up the current table
+					line = tableClose(tableEntries("d",line))
+
+				else:
+					#process 'line' and 'nextline' as rows in the table
+					line = tableBody(line)
+					nextline = tableBody(nextline)
+
+					line = line + nextline
+
+			if p:
+				line = "<p>" + line.rstrip() + " </p>\n"
+
+		#Match formatting markup
+			#Match a double **, for 'strong'
+			matches = tuple(re.findall(r'(?:\*{2}|_{2})',line))
+			if matches:
+				for x in matches:
+					#if markdown found, replace in 'line'
+					#you can also use '\g<n>' to match the nth saved group
+					#Match double *s or _s for 'strong'
+					line = re.sub(r'^(.*)\*{2}(.*?)\*{2}(.*)$',r'\1<strong>\2</strong>\3',line)
+					
+			#Match a double __, for 'strong'
+			matches = tuple(re.findall(r'_{2}',line))
+			if matches:
+				for x in matches:
+					#if markdown found, replace in 'line'
+					#you can also use '\g<n>' to match the nth saved group
+					#Match double *s or _s for 'strong'
+					line = re.sub(r'^(.*)_{2}(.*?)_{2}(.*)$',r'\1<strong>\2</strong>\3',line)
+					
+			#Match a single * not followed by a space, for 'emphasis'
+			matches = tuple(re.findall(r'\*(?! )',line))
+			if matches:
+				for x in matches:
+					#if markdown found, replace in 'line'
+					#you can also use '\g<n>' to match the nth saved group
+					#Match single *s for 'emphasis'
+					line = re.sub(r'^(.*)\*(?! )(.*?)\*(.*)$',r'\1<em>\2</em>\3',line)
+				
+			#Match a single _, for 'emphasis'
+			matches = tuple(re.findall(r'_',line))
+			if matches:
+				for x in matches:
+					#if markdown found, replace in 'line'
+					#you can also use '\g<n>' to match the nth saved group
+					#Match single *s for 'emphasis'
+					line = re.sub(r'^(.*)_(.*?)_(.*)$',r'\1<em>\2</em>\3',line)
+				
+			#Match a ~~, for 'strikethrough'
+			matches = tuple(re.findall(r'\~{2}',line))
+			if matches:
+				for x in matches:
+					#if markdown found, replace in 'line'
+					#you can also use '\g<n>' to match the nth saved group
+					#Match double ~~s for 'strikethrough'
+					line = re.sub(r'^(.*)\~{2}(.*?)\~{2}(.*)$',r'\1<s>\2</s>\3',line)
+				
+		#Match links
+			matches = tuple(re.findall(r'\]\(',line))
+			if matches:
+				for x in matches:
+					#if markdown found, replace in 'line'
+					#you can also use '\g<n>' to match the nth saved group
+
+					#Match: after left bracket, capture one or more non-bracket characters that are followed by a right bracket left parenthesis; then capture everything (non-greedy) after that right bracket left parenthesis up to the next right parenthesis
+					line = re.sub(r'\[([^\]]+)(?=\]\()\]\(([^\)]+)\)',r'<a href="\2">\1</a>',line)
+
+			#Now match raw links appearing in the line
+			if re.search(r'.*(?<!")http',line):
+				line = re.sub(r'(?<!")(http[^ ]+)',r'<a href="\1">\1</a>',line)
+
+		#Finally, detect any LaTeX-style math modes, and convert escaped characters
+			#Match exactly two literal dollar signs ('display math mode')
+			if re.search(r'\${2}',line):
+				line = re.sub(r'\${2}([^\$]+)\${2}',r'\\[ \1 \\]',line)
+
+			#Match single dollar signs ('inline math mode')
+			if re.search(r'\$',line):
+				line = re.sub(r'\$([^\$]+)\$',r'\\( \1 \\)',line)
+
+			#Replace escaped dollar signs with regular ones
+			if re.search(r'\\\$',line):
+				line = re.sub(r'\\\$',r'$',line)
+				
+			#Turn everything after a literal percent into an HTML comment
+			if re.search(r'(?<!\\)\%',line):
+				line = re.sub(r'(?<!\\)\%(.*)',r'<!--\1-->',line)
+				
+			#Replace escaped percent signs with regular ones
+			if re.search(r'\\\%',line):
+				line = re.sub(r'\\\%',r'%',line)
+
+			#Detect math mode in the line
+			if re.search(r'\\\(|\\\[',line): mathMode = True
+				
+			#write new 'line' out to file
+			g.write(line)
+
+#include mathjax header if math mode found in document
+mathjax = '\t<script type="text/javascript" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js">\n\t</script>' if mathMode else ''
+#Define the HTML header
+header = ("<!DOCTYPE html>\n\n"
+	"<html lang='en'>\n"
+	"<head>\n"
+	"	<title>"+insertHeaderInfo('title')+"</title>\n"
+	'	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> \n'
+	'	<meta name="viewport" content="width=device-width, initial-scale=1">\n'
+	'	<meta name="instructor" content="'+insertHeaderInfo('instructor')+'">\n'
+	'	<meta name="course" content="'+insertHeaderInfo('course')+'">\n'
+	'	<meta name="term" content="'+insertHeaderInfo('term')+'">\n'
+	'	<meta name="author" content="Steve Strand">\n'
+	'	<meta name="description" content="Syllabus for '+insertHeaderInfo('course')+', '+insertHeaderInfo('term')+'">\n'
+	#'\t	<link type="text/css" rel="stylesheet" href="CSS/teaching.css">\n'
+	'	<style>\n'
+	'		body, input {\n'
+	'			background-color: #222;\n'
+	'			color: #fff;\n'
+	'			max-width: 940px;\n'
+	'			font-size: 1.4vw;\n'
+	'			margin-left: 15px\n\t\t}\n'
+	'		a { color: cyan;}\n'
+	'	</style>\n'
+	+ mathjax +
+	'	\n'
+	"</head>\n\n"
+	"<body>\n"
+	"<h1>" + insertHeaderInfo('title') + "</h1>"
+	)
+
+#Write out header and closing tags
+with open(fileName + ".html","r") as f: data = f.read()
+with open(fileName + ".html","w") as g:
+	g.write(header+data+"\n</body>\n</html>")
---
+++ Syllabus-Template.md
@@ -0,0 +1,129 @@
+---
+title: Math &infin; Syllabus
+term: Spring 2020
+instructor: Phil Connors
+course: Math &infin;
+background: dark
+---
+
+# Probability and Statistics for Science and Technology
+
+Section 01 Holt 185, MoWeThFr 12:00PM - 12:50PM
+
+Section 02 Holt 189, MoWeThFr 3:00PM - 3:50PM
+
+## Contact Information and Office Hours
+
+[Edward A. Roualdes](mailto:eroualdes@csuchico.edu)
+
+Office Hours:
+
+Holt 204 Tu 12 - 12:50
+
+Community Coding in MLIB 442 Tu 2 - 3:50 and Th 2 - 2:50
+
+or by appointment
+
+## Textbook
+No textbook is required.
+
+* Most material will come from my (work in progress) [course notes](https://314.roualdes.us)
+* This is an pretty good, free, online book
+    - Soklaski, R. [Python Like You Mean It](https://www.pythonlikeyoumeanit.com/intro.html).
+* This is an excellent, free, online book that covers some good introductory data science material.
+    - Wickham, H. and Grolemund, G. [R for Data Science: Import, Tidy, Transform, Visualize, and Model Data.](http://r4ds.had.co.nz/). O’Reilly Media, Inc. 2016.
+* And this free, PDF book is a good resource for much of our vocabulary.
+    - D. M. Diez, C. D. Barr, and M. Cetinkaya-Rundel. [OpenIntro Statistics](https://www.openintro.org/download.php?file=os3&referrer=/stat/textbook.php), 3rd Edition. CreateSpace, 2015.
+* And this is a cool paper.
+    -  Taylor, S. J., and Letham, B. [Forecasting at Scale](https://peerj.com/preprints/3190.pdf)
+
+
+## Additional Requirements
+
+* Access to a computer will be essential to master the material of this course.
+* We will learn to code in [Python](https://www.python.org) using Jupyter Notebooks via [Anaconda](https://www.anaconda.com/distribution/).
+
+
+## Course Grading
+Your final grade for this course will be given according to the $+/-$ grading system, based on the following percentages and scale: $90 - 100$, A; $80 - < 90$, B; $70 - < 80$, C; $60 - < 70$, D; $<60$, F.
+
+| Component | Percentage |
+| ----------|------------|
+| Labs, Worksheets, Participation, and Quizzes | 15\% |
+| Homework | 15\% |
+| Exam 01 | 20\% |
+| Exam 02 | 20\% |
+| Final   | 30\% |
+
+
+## Homework
+
+All homework assignments will be submit to our GitHub Classroom no later than 12:00am on the date the assignment is due.  Working with other students on homework is allowed, subject to the Academic Integrity Policy below.  After the due date, you are allowed to turn in homework before the next exam for up to 50\% credit.  After this exam you will not be allowed to turn in late homework.
+
+On all homework, minus five million points for printing not-summarized data.
+
+
+## Tests
+There will be $3$ tests inclusive of the final.  Make-up tests are subject to the Make-Up Policy below.  All exams are comprehensive and will not be given earlier than the scheduled date for your class.
+
+## Make-Up Policy
+Course work can only be made-up in the case of a documented absence. To receive credit you must notify me in advance, or in the case of emergency, as soon as possible (within roughly 24 hours).  All undocumented absences will result in a zero.
+
+## Getting Help
+
+* Me -- though I reserve the right to refuse to provide help within
+  $24$ hours of an exam.
+
+* Community Coding -- TuWTh 2pm - 3:50pm in Yolo 206.
+
+* You can visit the Math Tutor Lab on the fourth floor of Meriam Library.   You should also visit your instructor during his/her office hours.
+
+
+## Diversity Policy
+Respect: Students in this class are encouraged to speak up and participate during class meetings. Because the class will represent a diversity of individual beliefs, backgrounds, and experiences, every member of this class must show respect for every other member of this class.
+
+## Academic Integrity Policy
+Students are permitted and encouraged to collaborate on all assignments other than examinations.  However, each student must turn in their own work.  Further, it is the expressed expectation of this instructor that all students demonstrate integrity and individual responsibility in all actions related to this course.  Unethical behavior of any kind is unacceptable and will be prosecuted vigorously.  Any sign of cheating in any way on any course exams or assignments will be addressed directly, according to university standards.  If you do not understand what plagiarism is, or what cheating entails, you must seek information regarding this matter from the current University Catalog and from me.  The consequences of plagiarism begin with a failing grade on the work, and possibly a failing grade in the course, depending upon university action.  More information is found at [Student Judicial Affairs](http://catalog.csuchico.edu/viewer/15/STUDJUDAFFAIRS.html).
+
+
+## Disability Support
+If you have any disability related needs in terms of taking exams or other accommodations, please contact Disability Support Service (Colusa Hall 898-5959 or campus information 898-INFO for directions) on campus to obtain the appropriate documentation.  Afterwards, come by my office and identify your needs within the first two weeks of class so that any necessary arrangements can be made.
+
+## Course Outline
+
+*  Discrete Uniform Distribution
+    -  Probability
+    -  Random Variables
+*  Estimating Proportions
+    -  goal of statistics
+    -  data basics
+*  The Bernoulli Distribution
+    -  estimating proportions
+*  Likelihood
+    -  optimization -- by hand and on a computer
+    -  other random varibles so as to practice the likelihood
+    -  variables, data structures, common functions, defining functions, and manipulating data
+  *  Random Variables
+    - random variables (discrete/continuous)
+    - probability distribution functions,
+    - parameters of distributions
+    - expectation, variance, and standard deviation
+*  Conditional probability
+    -  independence, sampling with/without replacement
+    -  dplyr -- group\_by, summarise, filter, mutate
+*  Sampling Distribution
+    -  sampling distribution, central limit theorem, confidence intervals
+*  Bootstrap
+    -  basic idea, standard error of mean
+*  Modeling
+    -  mean
+    -  means by group
+    -  hypothesis tests: paired, two sample t-tests, ANOVA
+    -  linear regression
+    -  logistic regression
+*  Prediciton Error
+    -  quantifying prediction error
+    -  training/testing prediction error
+*  Regularization
+    -  improving out of sample prediction error
+    -  k-fold cross validation
